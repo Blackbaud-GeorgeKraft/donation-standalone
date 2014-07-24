dd504b4a065a25ee3898f86f3ff67df6c8b709e3
George Kraft <george.kraft@blackbaud.com>
2014-07-24 15:01:37 -0500

    Connect the awesome intern UI to LO

--- static/donationForm.js
+++ static/donationForm.js
@@ -1,11 +1,11 @@
 $(function() {
-    // TODO: this shouldn't be hardcoded. That may be OK for demo purposes,
-    // but we'll need to adjust these values on different systems
     luminateExtend.init({
-        apiKey: 'whee', 
+        apiKey: 'whee',
         path: {
-            nonsecure: 'http://172.20.7.182/site/', 
+            nonsecure: 'http://172.20.7.182/site/',
             secure: 'https://172.20.7.182/site/'
         }
     });
+
+    luminateExtend.api.bind();
 });
